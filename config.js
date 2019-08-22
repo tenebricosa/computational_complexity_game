2b5f408f0aae3494cf18ad2974dddfb9f4847003
tenebricosa <elliot.wrt@gmail.com>
2019-08-22 11:39:52 +0500

    Добавила картинки в вопросах и тексты в ответах

---
+++ config.js
@@ -0,0 +1,22 @@
+﻿const config = {
+    level: "general",
+    time: 50000,
+    lives: 3
+};
+
+//IT-Nights
+
+const levels = {
+    "general": [
+    {
+        "name": "img/content/bubble-sort.gif",
+        "answers": [
+            "Сортировка пузырьком",
+            "Сортировка вставками",
+            "Быстрая сортировка",
+            "Сортировка слиянием",
+        ],
+        "right": 0,
+        "factor": 1
+    }
+]}
